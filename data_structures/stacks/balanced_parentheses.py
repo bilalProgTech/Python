7f4c5d6d145df1c775622388c0619b08f945623f
Bilal H. Hungund <hungundb401@gmail.com>
2019-12-08 17:42:58 +0530

    Two more braces are added i.e. { } and [ ], and update of example list with more complex braces
    If else condition remains the same.

--- data_structures/stacks/balanced_parentheses.py
+++ data_structures/stacks/balanced_parentheses.py
@@ -7,9 +7,17 @@ def balanced_parentheses(parentheses):
     """ Use a stack to check if a string of parentheses is balanced."""
     stack = Stack(len(parentheses))
     for parenthesis in parentheses:
-        if parenthesis == "(":
+        if parenthesis == "(" or parenthesis == "{" or parenthesis == "[":
             stack.push(parenthesis)
         elif parenthesis == ")":
+            if stack.is_empty():
+                return False
+            stack.pop()
+        elif parenthesis == "}":
+            if stack.is_empty():
+                return False
+            stack.pop()
+        elif parenthesis == "]":
             if stack.is_empty():
                 return False
             stack.pop()
@@ -17,7 +25,7 @@ def balanced_parentheses(parentheses):
 
 
 if __name__ == "__main__":
-    examples = ["((()))", "((())", "(()))"]
+    examples = ["((([{[]}])))", "((({[]}))", "((([)))","{{{(([])){}{}[[]]}}}",'{]{}}']
     print("Balanced parentheses demonstration:\n")
     for example in examples:
         print(example + ": " + str(balanced_parentheses(example)))
